28aac7b0aa5ce906f17e2aa589f8f803f55453c9
Jorge Aranda de Miguel <jorgearanda123@gmail.com>
2017-08-12 23:41:35 +0200

    Removing null fields from json

--- src/main/kotlin/es/jaranda/poc/springbootkolindemo/model/dto/response/DemoGreetingResponse.kt
+++ src/main/kotlin/es/jaranda/poc/springbootkolindemo/model/dto/response/DemoGreetingResponse.kt
@@ -1,8 +1,12 @@
 
 package es.jaranda.poc.springbootkolindemo.model.dto.response
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonInclude.*
+
 //import java.util.*
 
+@JsonInclude(Include.NON_NULL)
 class DemoGreetingResponse(
         //    var greet : Optional<String> = Optional.empty()
         var greet : String?,
