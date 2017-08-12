fb18bc3237b06297ef73caa1cee127d67a4a7da8
Jorge Aranda de Miguel <jorgearanda123@gmail.com>
2017-08-12 23:48:59 +0200

    Using ‘defaultGreet’ from a custom property

--- src/main/kotlin/es/jaranda/poc/springbootkolindemo/service/DemoService.kt
+++ src/main/kotlin/es/jaranda/poc/springbootkolindemo/service/DemoService.kt
@@ -2,6 +2,7 @@
 package es.jaranda.poc.springbootkolindemo.service
 
 import es.jaranda.poc.springbootkolindemo.model.dto.response.DemoGreetingResponse
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 interface DemoService {
@@ -10,9 +11,13 @@ interface DemoService {
 }
 
 @Service
-class DemoServiceImpl : DemoService {
-    override fun greet() = DemoGreetingResponse(greet = "Hello World!",
+class DemoServiceImpl(
+        @Value("\${es.jaranda.poc.springbootkolindemo.default-greet}")
+        val defaultGreet : String) : DemoService {
+
+    override fun greet() = DemoGreetingResponse(greet = defaultGreet,
                                                  name = null)
     override fun greet(greet: String, name: String?) =
             DemoGreetingResponse(greet = greet, name = name)
+
 }
