99b5d3d1abeaaad4b771336f47ea0f92fcdcf0a2
Jorge Aranda de Miguel <jorgearanda123@gmail.com>
2017-08-15 14:01:24 +0200

    Does Not Work: Trying to use Kotlin and Lombok on same project

--- src/main/kotlin/es/jaranda/poc/springbootkolindemo/service/DemoService.kt
+++ src/main/kotlin/es/jaranda/poc/springbootkolindemo/service/DemoService.kt
@@ -2,15 +2,19 @@
 package es.jaranda.poc.springbootkolindemo.service
 
 import es.jaranda.poc.springbootkolindemo.model.dto.response.DemoGreetingResponse
+import lombok.extern.slf4j.Slf4j
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 interface DemoService {
     fun greet() : DemoGreetingResponse
     fun greet(greet : String, name : String?) : DemoGreetingResponse
+    fun loggedGreet() : DemoGreetingResponse
+    fun loggedGreet(greet : String, name : String?) : DemoGreetingResponse
 }
 
 @Service
+@Slf4j
 class DemoServiceImpl(
         @Value("\${es.jaranda.poc.springbootkolindemo.default-greet}")
         val defaultGreet : String) : DemoService {
@@ -19,5 +23,14 @@ class DemoServiceImpl(
                                                  name = null)
     override fun greet(greet: String, name: String?) =
             DemoGreetingResponse(greet = greet, name = name)
+
+    override fun loggedGreet() = loggedGreet(defaultGreet, null)
+
+    override fun loggedGreet(greet: String, name: String?)
+            : DemoGreetingResponse {
+        val result = greet(greet, name)
+        log.info("Greet sent: '$result'")
+        return result
+    }
 
 }
