a92db1003e304f873a733738e18fc175d0a7934c
Jorge Aranda de Miguel <jorgearanda123@gmail.com>
2017-08-15 13:38:46 +0200

    Including reference to: https://stackoverflow.com/questions/34416869/idiomatic-way-of-logging-in-kotlin

--- src/main/kotlin/es/jaranda/poc/springbootkolindemo/Extensions.kt
+++ src/main/kotlin/es/jaranda/poc/springbootkolindemo/Extensions.kt
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory
 
 /**
  * Logger factory
+ *
+ * @see https://stackoverflow.com/questions/34416869/idiomatic-way-of-logging-in-kotlin
  */
 fun <R : Any> R.generateLogger(): Lazy<Logger> {
     return lazy { LoggerFactory.getLogger(this.javaClass) }
