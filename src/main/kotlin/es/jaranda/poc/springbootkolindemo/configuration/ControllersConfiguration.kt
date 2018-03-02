639936229e367263e8ef9e0f2ed72258e12daef3
Jorge Aranda de Miguel <jorgearanda123@gmail.com>
2018-03-02 17:26:05 +0100

    Controlling IllegalStateExceptions with a BAD REQUEST

---
+++ src/main/kotlin/es/jaranda/poc/springbootkolindemo/configuration/ControllersConfiguration.kt
@@ -0,0 +1,17 @@
+
+package es.jaranda.poc.springbootkolindemo.configuration
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import javax.servlet.http.HttpServletResponse
+
+@RestControllerAdvice
+class ControllersConfiguration {
+
+    @ExceptionHandler(IllegalStateException::class)
+    fun handleBadRequests(ex: Exception, response: HttpServletResponse) {
+        response.sendError(HttpStatus.BAD_REQUEST.value(), ex.message)
+    }
+
+}
