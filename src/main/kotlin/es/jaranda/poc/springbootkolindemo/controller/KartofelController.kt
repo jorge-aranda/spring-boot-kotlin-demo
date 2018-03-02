d2d7f0b284a5285c76f828c83c952ae0d6c163da
Jorge Aranda de Miguel <jorgearanda123@gmail.com>
2018-03-02 17:49:37 +0100

    Removing unnecesary blank line

--- src/main/kotlin/es/jaranda/poc/springbootkolindemo/controller/KartofelController.kt
+++ src/main/kotlin/es/jaranda/poc/springbootkolindemo/controller/KartofelController.kt
@@ -6,7 +6,6 @@ import es.jaranda.poc.springbootkolindemo.model.dto.request.KartofelCreationRequ
 import es.jaranda.poc.springbootkolindemo.service.KartofelService
 import org.springframework.web.bind.annotation.*
 
-
 @RequestMapping("kartofeln")
 @RestController
 class KartofelController(val kartofelService : KartofelService,
