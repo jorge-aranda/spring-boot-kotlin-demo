8318c73d29c7d6d2f30f0395ccee639eaa0cc849
Jorge Aranda de Miguel <jorgearanda123@gmail.com>
2018-03-03 12:49:27 +0100

    clean: Removing unnecesary TODO

--- src/main/kotlin/es/jaranda/poc/springbootkolindemo/model/domain/User.kt
+++ src/main/kotlin/es/jaranda/poc/springbootkolindemo/model/domain/User.kt
@@ -5,7 +5,6 @@ import es.jaranda.poc.springbootkolindemo.model.enums.UserRole
 
 data class User(
     val id : String,
-    // TODO username must be unique!!
     val username: String,
     val hashedPassword: String,
     val role: UserRole = UserRole.USER,
